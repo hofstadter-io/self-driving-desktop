cf2a0713f751dbb8ea5d649916071d06e3a746f7
Philip Dexter <philip.dexter@gmail.com>
2019-05-09 17:12:47 -0400

    Add setup.py
    Fix #1

---
+++ setup.py
@@ -0,0 +1,24 @@
+
+import setuptools
+
+with open('README.md', 'r') as f:
+  long_description = f.read()
+
+setuptools.setup(
+    name='self-driving-desktop',
+    version='0.0.1',
+    author='Hofstadter, Inc.',
+    author_email='support@hofstadter.io',
+    description='Desktop Automation Framework',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/hofstadter-io/self-driving-desktop',
+    packages=setuptools.find_packages(),
+    classifiers=[],
+    keywords=['desktop automation'],
+    install_requires=['click'],
+    entry_points='''
+    [console_scripts]
+    sdd=self_driving_desktop:drive
+    ''',
+    )
